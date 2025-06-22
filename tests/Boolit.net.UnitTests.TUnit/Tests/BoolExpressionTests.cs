8f35acacf6df3a9f243961cbea6c539d4dca55a5
GDCampbell <lonejock@gmail.com>
2025-06-23 00:35:23 +0100

    Added unit tests

---
+++ tests/Boolit.net.UnitTests.TUnit/Tests/BoolExpressionTests.cs
@@ -0,0 +1,50 @@
+﻿using Boolit.NET.Exceptions;
+
+namespace Boolit.NET.UnitTests.TUnit.Tests;
+
+internal sealed class BoolExpressionTests
+{
+    [Test]
+#pragma warning disable S1125 // Boolean literals should not be redundant
+#pragma warning disable S1764 // Identical expressions should not be used on both sides of operators
+#pragma warning disable S2437 // Unnecessary bit operations should not be performed
+    [Arguments("true and TRUE", (true && true))]
+    [Arguments("true and FALSE", (true && false))]
+    [Arguments("true or TRUE", (true || true))]
+    [Arguments("true or FALSE", (true || false))]
+    [Arguments("true xor TRUE", (true ^ true))]
+    [Arguments("true xor FALSE", (true ^ false))]
+    [Arguments("not true", !true)]
+    [Arguments("not false", !false)]
+    [Arguments("true and not true", (true && !true))]
+    [Arguments("true or not true", (true || !true))]
+    [Arguments("true xor not true", (true ^ !true))]
+    [Arguments("!true", !true)]
+    [Arguments("!(true and true) || (true ^ false) ", !(true && true) || (true ^ false))]
+    [Arguments("((false && true || true ^ true) || true ^ false)", ((false && true || true ^ true) || true) ^ false)]
+#pragma warning restore S2437 // Unnecessary bit operations should not be performed
+#pragma warning restore S1764 // Identical expressions should not be used on both sides of operators
+#pragma warning restore S1125 // Boolean literals should not be redundant
+    public async Task ValidExpression_ShouldEvaluateCorrectly(string inputExpression, bool expectedResult)
+    {
+        var expression = BoolExpression.Create(inputExpression);
+        var result = expression.Evaluate();
+        await Assert.That(result).IsEqualTo(expectedResult);
+    }
+
+    [Test]
+    [Arguments("true && and false")]
+    public async Task InvalidConsecutiveOperands_ShouldThrowInvalidConsecutiveOperandsException(string inputExpression)
+    {
+        var expression = BoolExpression.Create(inputExpression);
+        
+
+        var exception = await Assert.ThrowsAsync<InvalidConsecutiveOperandsException>(
+            async () => await Task.Run(() => expression.Evaluate())
+                .ConfigureAwait(false))
+            .ConfigureAwait(false);
+
+        await Assert.That(exception.Message)
+            .Contains("Invalid consecutive operands at index 8;");
+    }
+}
---
+++ tests/Boolit.net.UnitTests.TUnit/Tests/InsertMarkerTests.cs
@@ -0,0 +1,16 @@
+﻿using Boolit.NET.Extensions;
+
+namespace Boolit.NET.UnitTests.TUnit.Tests;
+internal sealed class InsertMarkerTests
+{
+    [Test]
+    [Arguments("true and && true", 9, "true and *&& true")]
+    [Arguments("(true or false) && true", 22,"(true or false) && true*")]
+    [Arguments("true or (false xor true && false", 22, "true or (false xor true* && false")]
+    public async Task MarkerIsInsertedAtCorrectIndex(string inputExpression, int index, string expectedExpression)
+    {
+        var result = inputExpression.InsertMarkerAtIndex(index);
+
+        await Assert.That(result).IsEqualTo(expectedExpression);
+    }
+}
