3599caebd691348aeb61a9fbb2f896e1c06c9936
GDCampbell <lonejock@gmail.com>
2025-06-23 01:41:18 +0100

    Update BoolExpressionTests.cs
    Added some additional test cases

--- tests/Boolit.net.UnitTests.TUnit/Tests/BoolExpressionTests.cs
+++ tests/Boolit.net.UnitTests.TUnit/Tests/BoolExpressionTests.cs
@@ -22,6 +22,10 @@ internal sealed class BoolExpressionTests
     [Arguments("!true", !true)]
     [Arguments("!(true and true) || (true ^ false) ", !(true && true) || (true ^ false))]
     [Arguments("((false && true || true ^ true) || true ^ false)", ((false && true || true ^ true) || true) ^ false)]
+    [Arguments("true && false || true", (true && false) || true)]
+    [Arguments("true || false && true", true || (false && true))]
+    [Arguments("   true   ", true)]
+    [Arguments("((((true))))", true)]
 #pragma warning restore S2437 // Unnecessary bit operations should not be performed
 #pragma warning restore S1764 // Identical expressions should not be used on both sides of operators
 #pragma warning restore S1125 // Boolean literals should not be redundant
